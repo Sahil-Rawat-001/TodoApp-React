1a510c9585d2573d3241afd43cba2e28c1aa4b59
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-02-17 15:26:24 +0530

    all file added for gh pages deployment

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([ ]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
